1e9f98e3baa54ddc78c03792b1c799c2e9e6abf1
Kraig Amador <kraig.amador@ticketmaster.com>
2018-06-22 16:24:02 -0700

    Tighten up loadbalancers data model to be less public, refactor into lb

--- pkg/annotations/validation_fake.go
+++ pkg/annotations/validation_fake.go
@@ -1,13 +1,13 @@
 package annotations
 
 type FakeValidator struct {
-	VpcId string
+	VpcId                             string
 	ResolveVPCValidateSubnetsDelegate func() error
-	ValidateSecurityGroupsDelegate func() error
-	ValidateCertARNDelegate func() error
-	ValidateInboundCidrsDelegate func() error
-	ValidateSchemeDelegate func() bool
-	ValidateWafAclIdDelegate func() error
+	ValidateSecurityGroupsDelegate    func() error
+	ValidateCertARNDelegate           func() error
+	ValidateInboundCidrsDelegate      func() error
+	ValidateSchemeDelegate            func() bool
+	ValidateWafACLIDDelegate          func() error
 }
 
 func (fv FakeValidator) ResolveVPCValidateSubnets(a *Annotations) error {
@@ -46,9 +46,9 @@ func (fv FakeValidator) ValidateScheme(a *Annotations, ingressNamespace, ingress
 	return true
 }
 
-func (fv FakeValidator) ValidateWafAclId(a *Annotations) error {
-	if fv.ValidateWafAclIdDelegate != nil {
-		return fv.ValidateWafAclIdDelegate()
+func (fv FakeValidator) ValidateWafACLID(a *Annotations) error {
+	if fv.ValidateWafACLIDDelegate != nil {
+		return fv.ValidateWafACLIDDelegate()
 	}
 	return nil
 }
